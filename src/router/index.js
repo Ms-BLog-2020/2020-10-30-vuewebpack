bbc7b0f5198fa2fb263b315b73b79fd3045fcbbf
sandy <a68b58@gmail.com>
2020-10-31 11:18:28 +0800

    增加分頁路徑 Page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '@/components/HelloWorld';
+import Page from '@/components/pages/page';
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -10,5 +11,11 @@ export default new VueRouter({
             name: '首頁',
             path: '/index',
             component: Home,
-        }
+        },
+        {
+            name: '分頁',
+            path: '/page',
+            component: Page,
+        },
     ]
+})
